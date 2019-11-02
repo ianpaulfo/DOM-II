1ae9e4e9b38f1d679486d5e02aafebf3493b6c06
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-11-01 21:29:29 -0700

    added 4th event listener

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 
 //1. 1st adventure image will scale up when you mouse over
 const adventureImg = document.querySelector('.img-content');
-console.log('advenstureImg', adventureImg);
 
 adventureImg.addEventListener("mouseenter", () => {
   adventureImg.style.transform="scale(1.2)";
@@ -20,11 +19,36 @@ const jumpImg = document.querySelector('.intro img');
    
 jumpImg.addEventListener('dblclick', () => {
     jumpImg.src = "img/doran-erickson-gdAuwo-qj5k-unsplash.jpg";
+});
+
+//3. change nav color when clicked
+
+const navRed = document.querySelectorAll('.nav-link')[0];
+  navRed.addEventListener('click', () => {
+    navRed.style.color = 'red';
+  });
+  const navBlue = document.querySelectorAll('.nav-link')[1];
+  navBlue.addEventListener('click', () => {
+    navBlue.style.color = 'blue';
+  });
+  const navGreen = document.querySelectorAll('.nav-link')[2];
+  navGreen.addEventListener('click', () => {
+    navGreen.style.color = 'green';
+  });
+  const navOrange = document.querySelectorAll('.nav-link')[3];
+  navOrange.addEventListener('click', () => {
+    navOrange.style.color = 'orange';
+  });
+
+//4.
+
+const selectHeader = document.querySelectorAll(".btn");
+selectHeader.forEach(button => {
+button.addEventListener('mousedown', () => {
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+  });
 })
 
-//3.
-// cont redTxt = document.querySelectorAll('.nav');
+//5.
 
-// redTxt.addEventListener("mouseover", () => {
-//     redTxt.style.backgroundColor = "red";
-// })
